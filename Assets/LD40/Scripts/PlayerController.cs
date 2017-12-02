619d3d82f88d1b86f3e0f2edcd2c3899a9f26a03
JPiolho <jpiolhox@gmail.com>
2017-12-02 10:08:01 +0100

    Added Player controller

---
+++ Assets/LD40/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int health;
+    public PlayerController controller;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            var characterController = this.GetComponent<CharacterController>();
+
+            characterController.Move(Vector2.down * Time.deltaTime);
+        }
+
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+    }
+
+
+
+}
